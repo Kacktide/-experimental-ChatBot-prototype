e564c629a9bde088d8408916fb7e701f7077e9c7
FURY69 <70317531+Kacktide@users.noreply.github.com>
2023-05-21 07:47:58 +0545

    Update response.js

--- assets/js/response.js
+++ assets/js/response.js
@@ -6,4 +6,9 @@ const res = [
   ["Hi!.. How are you?", "Hello!.. How do you do?"],
   ["I'm good!.. and you?", "I'm very good"],
 ];
-const err = [["Uhm.. What?", "Try again.. pls"]];
+const err = [
+  "Uhm.. What?",
+  "Try again.. please!",
+  "I'd better expect some clarity in your speech.",
+  "Well, I don't know how to react to this..",
+];
