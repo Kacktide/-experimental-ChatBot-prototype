43cafc6574c189a6bab1cc89fd040d43f7f96502
FURY69 <70317531+Kacktide@users.noreply.github.com>
2023-05-21 07:47:15 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,49 @@
 const chatInput = document.querySelector("#chat-input");
 const canvasArea = document.querySelector(".canvas-area");
 const chatMsgArea = document.querySelector(".chat-msg-area");
+const chatForm = document.querySelector("#asd");
+
+const userMessage = "user-msg";
+const botMessage = "bot-msg";
 
 const randomize = (a) => {
-  Math.floor(Math.random() * a.length);
+  return Math.floor(Math.random() * a.length);
 };
 
-const createMessage = (value) => {
+const createMessage = (value, msgType) => {
   const newElem = document.createElement("div");
-  newElem.textContent = value;
-  newElem.setAttribute("class", "chat-msg");
+  newElem.innerHTML = value;
+  newElem.setAttribute("class", "chat-msg , " + msgType);
   chatMsgArea.append(newElem);
 };
 
 const mainFnc = chatInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter" && chatInput.value !== "") {
+  if (e.key === "Enter" && chatInput.value != "") {
     e.preventDefault();
     const textVal = chatInput.value;
-    createMessage(textVal);
+    createMessage(textVal, userMessage);
     chatInput.value = "";
     textVal.toLowerCase().replace(/d/gi, "");
     execMsg(arg, res, textVal);
+    scrollToMsg();
   }
 });
-
+const scrollToMsg = () => {
+  const chatScrollLength = chatMsgArea.scrollHeight;
+  chatMsgArea.scrollTo(0, chatScrollLength);
+};
 const execMsg = (req, res, input) => {
+  let isSuccess = false;
   for (let a = 0; a < req.length; a++) {
     for (let b = 0; b < req[a].length; b++) {
       if (req[a][b] === input) {
-        let resOutput = res[a][randomize(b)];
-      } else {
-        let resErr = err[randomize(err)];
+        createMessage(res[a][randomize(res[a])], botMessage);
+        isSuccess = true;
+        break;
       }
     }
+  }
+  if (!isSuccess) {
+    createMessage(err[randomize(err)], botMessage);
   }
 };
